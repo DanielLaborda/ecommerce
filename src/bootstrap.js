70b485262441607cc16f6e3c6769239d56c48cf1
DanielLaborda <dani.laborda90@gmail.com>
2021-04-13 21:01:48 +0200

    styled details quick links component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
+import { Router, Switch, Route } from  'react-router-dom';
 
 import SignIn from './components/auth/signin';
 import SignUp from "./components/auth/signup";
 
-import thunk from 'redux-thunk';
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
