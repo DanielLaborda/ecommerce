bcb4723ec534a3a588d65bc0e4554f2e4f92d5c8
DanielLaborda <dani.laborda90@gmail.com>
2021-04-15 19:40:33 +0200

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -9,12 +9,92 @@ export function fetchUserPurchases() {
             {
                 _id:0,
                 title: 'purchase 1',
-                amount: 8.02
+                amount: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Daniel Laborda',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id:1,
-                title: 'purchase 2',
-                amount: 19.40
+                amount: 19.40,
+                orderNumber: 'A0034534565',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Maxwell Nelson',
+                    shippingAddress: '3333 West State Street'
+                }
+            },
+            {
+                _id:2,
+                amount: 14.38,
+                orderNumber: 'A004242334',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id:3,
+                amount: 25.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Tony Gambino',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id:4,
+                title: 'purchase 1',
+                amount: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Daniel Laborda',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id:5,
+                amount: 19.40,
+                orderNumber: 'A0034534565',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Maxwell Nelson',
+                    shippingAddress: '3333 West State Street'
+                }
+            },
+            {
+                _id:6,
+                amount: 14.38,
+                orderNumber: 'A004242334',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id:7,
+                amount: 25.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Tony Gambino',
+                    shippingAddress: '1234 West State Street'
+                }
             }
         ]
     })
--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -8,9 +8,13 @@ class PurchaseHistory extends Component {
   render() {
     return (
       <div className='purchase-history'>
-         <PageTitle className='purchase-history__page-title' title='Purchase History'/>
-         <Purchases className='purchase-history__purchases'/>
-         <PurchaseDetails className='purchase-history__purchase-detail'/>
+          <PageTitle className='purchase-history__page-title' title='Purchase History'/>
+          <div className='purchase-history__content'>
+            <Purchases className='purchase-history__purchases'/>
+            <PurchaseDetails className='purchase-history__detail'/>
+          </div>
+
+         
          <div className='purchase-history__border-bottom'></div>
       </div>
     );
